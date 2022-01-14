c54924be0178c15f6369e992b52c9e9d9233760f
Daniel Lipec <dl133218@ohio.edu>
2022-01-13 23:22:16 -0500

    fixed something

--- filesize.cc
+++ filesize.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 // output format to use IDENTICALLY:
@@ -8,11 +9,39 @@ using namespace std;
 // input.3lines: 3
 // input.1line: 1
 
+int countLines(string filename);
+
 int main( int argc, char* argv[] )
 {
+    std::cout << "./filesize" << '\n';
+
     // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+    for (int arg = 1; arg < argc; ++arg) {
+        string filename;
+        filename.assign(argv[arg]);
+        std::cout << argv[arg] << ": " << countLines(filename) << '\n' ;
+    }
 
     exit(0); // this means that the program executed correctly!
+}
+
+int countLines(string filename) {
+    int lines = -1;
+    string line;
+
+    ifstream file;
+    file.open(filename);
+
+    if (!file.is_open()) {
+        return -1;
+        
+    }
+
+    do {
+        lines++;
+        getline(file, line);
+    } while (!file.eof());
+
+    return lines;
+
 }
